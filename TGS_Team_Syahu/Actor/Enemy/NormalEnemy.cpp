2314173a60bc058bb7e797e65f01fec769ce3d45
Nakora-koutyan <104492122+Nakora-koutyan@users.noreply.github.com>
2024-04-12 17:42:32 +0900

    Enemyの動きの修正

--- TGS_Team_Syahu/Actor/Enemy/NormalEnemy.cpp
+++ TGS_Team_Syahu/Actor/Enemy/NormalEnemy.cpp
@@ -46,6 +46,9 @@ void NormalEnemy::Update(GameMainScene* object)
 	//プレイヤーを検知するセンサー
 	DiscoveryPlayer();
 
+	//プレイヤーにダメージを与える処理
+	GiveDamage(object);
+
 	if (attack_range[0].x < object->GetPlayer()->GetMaxLocation().x &&
 		attack_range[1].x > object->GetPlayer()->GetMinLocation().x)
 	{
@@ -60,22 +63,30 @@ void NormalEnemy::Update(GameMainScene* object)
 //描画に関する更新
 void NormalEnemy::Draw() const
 {
+	//エネミー
 	DrawBoxAA
 	(
 		screenLocation.x, screenLocation.y,
 		screenLocation.x + area.width, screenLocation.y + area.height,
 		isHit ? attack_color : usual_color, TRUE, 1.0f
 	);
+	//攻撃範囲表示
+	DrawBoxAA
+	(
+		attack_range[0].x,attack_range[0].y - 45,
+		attack_range[1].x,attack_range[1].y + 45,
+		GetColor(255,0,255),FALSE,1.f
+	);
 }
 
 //プレイヤーのいる方向に向かう
 void NormalEnemy::EnemyPatrol(GameMainScene* player)
 {
-	if (player->GetPlayer()->GetCenterLocation().x < screenLocation.x)
+	if (player->GetPlayer()->GetCenterLocation().x < location.x)
 	{
 		vector.x = ENEMY_SPEED;
 	}
-	else if (player->GetPlayer()->GetCenterLocation().x >= screenLocation.x)
+	else if (player->GetPlayer()->GetCenterLocation().x >= location.x)
 	{
 		vector.x = -ENEMY_SPEED;
 	}
@@ -99,8 +110,10 @@ void NormalEnemy::DiscoveryPlayer()
 
 float NormalEnemy::GiveDamage(GameMainScene* player_hp)
 {
+	hp = player_hp->GetPlayer()->GetHp();
 	if (isHit == true)
 	{
-
+		hp -= GIVE_DAMAGE;
 	}
+	return player_hp->GetPlayer()->GetHp();
 }
