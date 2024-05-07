b833f94bbf02f31b1dc4a166c6eefc6535378919
nigirimeshi02 <104492636+nigirimeshi02@users.noreply.github.com>
2024-05-07 15:19:22 +0900

    ResourceManagerの追加とレイピアの追加（まだ未完成）

--- TGS_Team_Syahu/Utility/common.h
+++ TGS_Team_Syahu/Utility/common.h
@@ -8,7 +8,7 @@
 #define WORLD_WIDTH		2560								//ワールドの横幅
 #define WORLD_HEIGHT	1440								//ワールドの縦幅
 
-#define GROUND_LINE		WORLD_HEIGHT - 100	//地面
+#define GROUND_LINE		WORLD_HEIGHT - 100					//地面
 
 #define DEGREE_TO_RADIAN(_deg)	_deg * (DX_PI_F / 180.f)		//角度からラジアンへ
 #define RADIAN_TO_DEGREE(_rad)	_rad * (180.f / DX_PI_F)		//ラジアンから角度へ
